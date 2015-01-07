745d021bbf3474f386a62ed63ae51c4c753dd22d
fvogel <fps.vogel@gmail.com>
2015-01-06 19:27:01 -0500

    ee 20

--- js/main.js
+++ js/main.js
@@ -12,6 +12,13 @@
 		file: {
 			name: 'maccer',
 			defaultContent: text
+		},
+		theme: {
+			preview: '/themes/preview/preview-light.css',
+			editor: '/themes/editor/epic-light.css'
+		},
+		button: {
+			fullscreen: false,
 		}
 	};
 	var editor = new EpicEditor(options).load();
