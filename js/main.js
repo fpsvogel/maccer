85062d33f30cccbe068b59e6cd472049d38fc067
fvogel <fps.vogel@gmail.com>
2015-01-06 19:13:34 -0500

    ee 12

--- js/main.js
+++ js/main.js
@@ -4,35 +4,22 @@
 // Default content to display to EpicEditor
 
 	var text = [
-		"# Introducing EpicEditor",
-		"",
-		"This is a demo site which contains examples on bending EpicEditor to your will. Be it text formatting, interacting with `Selection` and `Range` objects or even implementing your own UI elements – this page serves to demonstrate some of the way you can use EpicEditor.",
-		"",
-		"## Text Formatting, almost WYSIWYG",
-		"",
-		"In this section, we'll have a look at using existing EpicEditor API methods to implement a simple toolbar which makes texts bold, italic and etc. If you haven't realised by now, there is a very simple toolbar presented above the editor. Try selecting text in this editor and press any one of those buttons.",
-		"",
-		"Item1",
-		"Item2",
-		"Item3",
-		"",
-		"### Relevant source codes",
-		"",
-		"```",
-		"javascript",
-		"public/js/toolbar.js",
-		"public/js/commands.js",
-		"```",
-		"",
-		"Take a peek at them and see how those buttons are implemented!"
+		"Enter Latin text here."
 	].join('\n');
 
 
 	var options = {
 		file: {
-			name: 'demo',
+			name: 'maccer',
 			defaultContent: text
 		}
+		theme: {
+			preview: '/themes/preview/preview-light.css',
+			editor: '/themes/editor/epic-light.css'
+		},
+		button: {
+			fullscreen: false,
+		},
 	};
 	var editor = new EpicEditor(options).load();
 	var commands = window.DefaultCommands;
